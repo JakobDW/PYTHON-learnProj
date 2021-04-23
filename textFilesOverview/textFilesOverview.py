eefbd4bcad6394aba56f8028650280a8096cd795
JakubDwornicki <82828639+JakobDW@users.noreply.github.com>
2021-04-23 16:54:52 +0200

    textFileOverview v 0.4 + v0.6
    0.4  - Separate text files from the list
    
    0.6 - Define regural expression
          - Look through lines of files and save line if mach inside has been found

--- textFilesOverview/textFilesOverview.py
+++ textFilesOverview/textFilesOverview.py
@@ -3,14 +3,14 @@
 #                       all text files
 #                       in user defined directory
                         
-#***************************************************************************************************************************************************
+################################################################################################################################################################
 
 import re, os
 
 print('Current working directory:')                                                                             #Print cwd
 print(os.getcwd())
 print()
-
+################################################################################################################################################################
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------
 #Loop to decide cwd
 
@@ -43,13 +43,62 @@ while(True):                                                                    
             print('Incorrect cwd path')
             print()
     else:                                                                                                       #If cwd not to be changed break
+        print()
         break
 
 #End loop to decide cwd
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------
-
+################################################################################################################################################################
+print('Current working directory:')
 print(os.getcwd())
+print()
 
-fileList = os.listdir(os.getcwd())
+fileList = os.listdir(os.getcwd())                                                                              #get list of files from cwd
+################################################################################################################################################################
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Select text files from fileList
 
-print(fileList)
+txtFileList = []
+
+regEx = re.compile('\.txt')
+
+for file in range(len(fileList)):                                                                                #For every file on file list
+
+    res = regEx.search(fileList[file])                                                                           #Search for .txt extension
+
+    if res != None:                                                                                              #If .txt extension is present in searched file name
+        txtFileList.append(fileList[file])
+
+#End text file selection 
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------
+################################################################################################################################################################
+if not txtFileList:
+    print('There are no text files in cwd')
+    print()
+################################################################################################################################################################
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Define regEx you want to look for in those files and 
+else:
+    print('Define regular expression to look for(remember of \'\')')                                             #As described
+    reg = input()
+
+    regEx = re.compile(reg)
+
+    discMachLines={}
+
+    for file in txtFileList:                                                                                     #For every file on txtFileList
+        openFile = open(file,r)                                                                                  #Open to read
+        fileInLines = openFile.redlines()                                                                        #Save to list in lines
+        openFile.close()                                                                                         #Close
+        for line in fileInLines:                                                                                 #For every line in fileInLines
+            res = regEx.search(line)                                                                             #Search for maching regular expression
+            if res != None:                                                                                      #If found mach
+                discMachLines[file]+=line                                                                        #Save line under according key in dictionary
+
+#End of searchng through text files lines
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------
+################################################################################################################################################################
+
+      
+
+
