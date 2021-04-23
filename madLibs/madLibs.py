7f9add1aad0b549d4382eb4f97f94fbb16be29b5
JakubDwornicki <82828639+JakobDW@users.noreply.github.com>
2021-04-23 10:22:29 +0200

    madLibs v 2.0
    Final build. Problem was incorrect indexing of keyList.
    indexing starting from 0 and len(keysList) is n+1.

--- madLibs/madLibs.py
+++ madLibs/madLibs.py
@@ -2,20 +2,20 @@
 
 import re
 
-def swaper(keyList, ansDict, sentence):
+def swaper(keyList, ansDict, sentence):                                    #swap templates with user answers
     
-    newSen =''
+    newSen = sentence
 
-    for i in range(len(keyList)):
+    for i in range(len(keyList)):                                          #for every template
 
        
-       spl = sentence.split(keyList[i])
+       spl = newSen.split(keyList[i-1])                                    #split str on TEMPLATE
 
-       newSen = spl[0] + ansDict[keyList[i]] + spl[1]
+       newSen = spl[0] + ansDict[keyList[i-1]] + spl[1]                    #insert user answer between splitted str fragments
 
     return newSen
 
-sentenceFile = open('sentence.txt','r')
+sentenceFile = open('sentence.txt','r')                                    
 
 sentence = sentenceFile.read() 
 
@@ -25,7 +25,7 @@ print(sentence)
 
 #TODO ask user to define swaps for NOUN, ADJECTIVE, VERB and ADVERB
 
-words = {'NOUN':'','ADJECTIVE':'','VERB':'','ADVERB':''}
+words = {'NOUN':'','ADJECTIVE':'','VERB':'','ADVERB':''}                   #dict holds templates tied to user answers
 
 print('Provide noun:')
 words['NOUN'] = input()
@@ -43,10 +43,9 @@ words['ADVERB'] = input()
 #TODO find those in text and swap them with user definitions
 
 
-keysList = list(words.keys())
+keysList = list(words.keys())                                               #Convert keys to list of keys
 
-
-finalMess = swaper(keysList, words, sentence)
+finalMess = swaper(keysList, words, sentence)                               #Cal swaper function
 
 #TODO print the results
 
