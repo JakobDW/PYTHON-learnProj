16e8c361d2a054bad4e59cb1aac3988cd6f19050
JakubDwornicki <82828639+JakobDW@users.noreply.github.com>
2021-05-07 14:53:17 +0200

    2048 v 1.2
    - Game is played in loop 20 times

--- 2048/_2048.py
+++ 2048/_2048.py
@@ -8,8 +8,22 @@ browser.get('https://play2048.co/')
 
 htmlElem = browser.find_element_by_tag_name('html')
 
+
+count = 0
+
 while True:
     htmlElem.send_keys(Keys.UP)
     htmlElem.send_keys(Keys.RIGHT)
     htmlElem.send_keys(Keys.DOWN)
     htmlElem.send_keys(Keys.LEFT)
+    try:
+        retryButton = browser.find_element_by_class_name('retry-button')
+        retryButton.click()
+        count+=1
+    except:
+        print('Ze GAME continues...')
+    if count == 20:
+        break
+
+
+    
